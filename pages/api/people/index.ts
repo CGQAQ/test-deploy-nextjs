cc73f46bd7ba9f315f74efdf44497419e4829f30
CGQAQ <m.jason.liu@outlook.com>
2023-02-14 17:11:31 +0800

    log current dir

--- pages/api/people/index.ts
+++ pages/api/people/index.ts
@@ -2,10 +2,14 @@ import { NextApiResponse, NextApiRequest } from 'next'
 import { people } from '../../../data'
 import { Person } from '../../../interfaces'
 
+import {readdirSync} from "node:fs"
+
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Person[]>
 ) {
-  console.log("????????", _req);
+  console.dir("????????", _req);
+
+  console.log("ls", readdirSync(process.cwd()));
   return res.status(200).json(people)
 }
